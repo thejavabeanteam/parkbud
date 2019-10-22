3820ee9bbdfc0fde75876689f8ca3115a6cdfb8a
Mora <mlabisi98@gmail.com>
2019-10-22 11:23:03 -0700

    Updated App.js boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-// import logout component and the Menu
+// import logout store
 // import the Menu component
 
 /**
