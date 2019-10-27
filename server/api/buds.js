093034e26d379a66d5de3f215a51d5cb5f196e82
Mora <mlabisi98@gmail.com>
2019-10-27 15:47:41 -0700

    Added /buds endpoint

---
+++ server/api/buds.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+module.exports = router;
+
+router.get('/', (req, res, next) => {
+    // use a query string/user pref's to display buds with matching schedule
+    // and matching parking lot preference
+});
+
+router.get('/findById/:userId', (req, res, next) => {
+    // view a user's profile
+});
--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 module.exports = router;
 
 router.use('/userAccount', require(' ./userAccount'));
+
 router.use('/match', require('./match'));
+router.use('/buds', require('./buds'));
 
 router.use((req, res, next) => {
     const error = new Error('Not Found');
