5405f774ecb7e14a84e85abaf82262552f46b434
Mora <mlabisi98@gmail.com>
2019-10-27 15:30:24 -0700

    Added /match endpoint and modified stubs for /userAccount

--- server/api/userAccount.js
+++ server/api/userAccount.js
@@ -1,18 +1,17 @@
 const router = require('express').Router();
 module.exports = router;
 
-// get a single user (to set them as current user on state)
 router.get('/:userId', (req, res, next) => {
-    // find the user with the given userId
+    // get a single user (to set them as current user on state)
+
 });
 
-// update a user's profile
 router.put('/:userId', (req, res, next) => {
     // replace the given user's info with the updated info
 });
 
-//delete a user
 router.delete('/:userId', (req, res, next) => {
-    // destory the given user
+    // delete a user
+
 });
 
