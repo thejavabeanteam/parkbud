2d4d8eb9ac4a30496b483369a91cca0d75a43b7f
chauaihuynh <chauaihuynh@gmail.com>
2019-10-28 01:49:15 -0700

    Added components: CurrentLocation and UserHome

---
+++ client/src/components/user-home.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { deleteAccount } from '../store';
+import Matches from './Matches';
+
+// COMPONENT
+
+export const UserHome = (props) => {
+    const { user, deleteUser } = props;
+    const userId = user.id;
+    return (
+        <div className="userProfile">
+            <h1>My Profile</h1>
+            <div className="userInfo">
+                <div>
+                    <h4>Name:</h4>
+                    <p>{user.name}</p>
+                </div>
+                <div>
+                    <h4>Email:</h4>
+                    <p>{user.email}</p>
+                </div>
+                <div>
+                    <h4>Phone Number:</h4>
+                    <p>{user.phoneNumber}</p>
+                </div>
+                <div>
+                    <h4>Zip Code:</h4>
+                    <p>{user.zipCode}</p>
+                </div>
+                <div>
+                    <h4>Parking Spot Preference:</h4>
+                    {!user.spotPreferences.length
+                        ? <p>None</p>
+                        : <ul>{user.spotPreferences.map((spot, index) =>
+                            <li key={index}>{spot}</li>)}
+                        </ul> }
+                </div>
+                <div>
+                    <h4>Current Vehicle:</h4>
+                    <p>{user.vehicle}</p>
+                </div>
+            </div>
+            <div className="userButtons">
+                <NavLink to="/updateProfile">
+                    <button>Edit Your Profile</button>
+                </NavLink>
+                <NavLink to="/login">
+                    <button id="deleteUser" onClick={event => deleteUser(event, userId)}>Delete</button>
+                </NavLink>
+            </div>
+            <Matches userId={props.user.id} />
+        </div>
+    );
+};
+
+
+// CONTAINER
+const mapState = state => ({ user: state.currentUser });
+
+const mapDispatch = dispatch => ({
+    deleteUser(event, userId) {
+        event.preventDefault();
+        if (window.confirm('Are you sure you want to delete your account?')) {
+            dispatch(deleteAccount(userId));
+        }
+    },
+});
+
+
+export default connect(mapState, mapDispatch)(UserHome);
+
+/**
+ * PROP TYPES
+ */
+UserHome.propTypes = {
+    email: PropTypes.string,
+};
