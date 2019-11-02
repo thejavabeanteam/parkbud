f7c7fc7f8fbea54aff47771d3bc317419be3508b
Mora <mlabisi98@gmail.com>
2019-11-01 19:46:08 -0700

    Tested /api/match with postman

--- server/api/match.js
+++ server/api/match.js
@@ -4,43 +4,41 @@ const {
 } = require('../db/models');
 module.exports = router;
 
-//Get all matches belonging to the logged-in user-- requires the user to be logged in so the currentUser's id can be passed to req.body
+//Get all matches belonging to the logged-in user
+// requires the user to be logged in so the currentUser's id can be passed to req.body for post
 router.get('/:userId', (req, res, next) => {
-    /*
-    route format:
-      `.../api/match/${userId}`
-    */
     Match.findAll({
         where: {
-            currentUserId: req.params.userId
+            currentId: req.params.userId
         }
     })
         .then(matches => res.json(matches))
         .catch(next)
 });
 
-//Add a match to a logged-in user
+//Add a match to the logged-in user
 router.post('/', (req, res, next) => {
     Match.findOrCreate({
         where: {
             currentId: req.body.currentId,
             matchId: req.body.matchId
         }
-    })
+    }).then(Match.findOrCreate({
+        where: {
+            currentId: req.body.matchId,
+            matchId: req.body.currentId
+        }
+    }))
         .then((newMatch) => res.json(newMatch[0]))
         .catch(err => console.log(err))
 });
 
 // on Contact set the match.contacted to true
 router.put('/:currentId/:matchId', (req, res, next) => {
-    /*
-    route format:
-      `.../api/match/${currentId}/${matchId}`
-    */
     Match.update({contacted : true}, {
         where: {
-            currentId: req.body.currentId,
-            matchId: req.body.matchId
+            currentId: req.params.currentId,
+            matchId: req.params.matchId
         }
     })
         .then(result => res.json(result))
@@ -53,7 +51,12 @@ router.delete('/', (req, res, next) => {
             currentId: req.body.currentId,
             matchId: req.body.matchId
         }
-    })
+    }).then(Match.destroy({
+            where: {
+                currentId: req.body.matchId,
+                matchId: req.body.currentId
+            }
+        }))
         .then(() => res.sendStatus(204))
         .catch(next)
 });
