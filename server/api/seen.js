41411c21797a131e7ac7a7041fbdf86f6a5d7302
Mora <mlabisi98@gmail.com>
2019-11-01 22:58:20 -0700

    Tested /api/seen with postman

--- server/api/seen.js
+++ server/api/seen.js
@@ -2,23 +2,18 @@ const router = require('express').Router();
 const {Seen} = require('../db/models');
 module.exports = router;
 
-//Get all Seens belonging to the logged-in user-- requires the user to be logged in so the currentUser's id can be passed to req.body
-router.put('/:currentId/:matchId', (req, res, next) => {
-    /*
-    route format:
-      `.../api/match/${currentId}/${matchId}`
-    */
+// Get all Seens belonging to the logged-in user
+// requires the user to be logged in so the currentUser's id can be passed to req.body for post
+router.get('/:currentId', (req, res, next) => {
     Seen.findAll({
         where: {
-            currentId: req.body.currentId,
-            matchId: req.body.matchId
-        }
+            currentId: req.params.currentId}
     })
         .then(seens => res.json(seens))
         .catch(next)
 });
 
-//Add a Seen to a logged-in user-- expects an object with petId and userId
+// Add a Seen to a logged-in user
 router.post('/', (req, res, next) => {
     Seen.create({
         currentId: req.body.currentId,
