1f8dd421517b54fb9ca1f89f54fb1c03017f7a5b
Mora <mlabisi98@gmail.com>
2019-11-12 13:16:53 -0800

    Finished code review

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-// import index.css once it's created
-import 'client/src/styles/index.scss';
-// import api routes once they're created
+import {Provider} from 'react-redux';
+import 'client/src/styles/index.css';
 import Routes from './routes';
-// import redux store once it's created
 import store from './store';
 
 ReactDOM.render(
-// Grab the store from redux using the Provider object
-// Display the api routes in order to load the app
     <Provider store={store}>
-        <Routes />
+        <Routes/>
     </Provider>,
-
-
-    document.getElementById('root'));
+document.getElementById('root'));
