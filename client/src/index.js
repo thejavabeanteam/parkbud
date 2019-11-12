8614585eaab2fa83af72292b6838aafcd4454ecc
Ronald <ronaldtran97>
2019-11-12 11:45:55 -0800

    I think I did this right? There are some errors I dont completely understand T_T

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 // import index.css once it's created
+import 'client/src/styles/index.scss';
 // import api routes once they're created
+import Routes from './routes';
 // import redux store once it's created
+import store from './store';
 
 ReactDOM.render(
 // Grab the store from redux using the Provider object
 // Display the api routes in order to load the app
+    <Provider store={store}>
+        <Routes />
+    </Provider>,
 
-document.getElementById('root'));
+
+    document.getElementById('root'));
